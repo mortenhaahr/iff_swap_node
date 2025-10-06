afb48dde550b8bea7408672325716e83bfca07f9
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2025-10-06 14:24:32 +0200

    Done until tomorrow

--- swap_node/src/swap_node.cpp
+++ swap_node/src/swap_node.cpp
@@ -4,36 +4,64 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
+using std::placeholders::_1;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
-
-class MinimalPublisher : public rclcpp::Node {
+class SwapNode : public rclcpp::Node {
 public:
-  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+  SwapNode() : Node("switch_forwarder"), use_yolo(false) {
+    // Subscribers
+    intel_sub = this->create_subscription<std_msgs::msg::String>(
+        "intel_data", 10, std::bind(&SwapNode::intel_callback, this, _1));
+
+    yolo_sub = this->create_subscription<std_msgs::msg::String>(
+        "yolo_data", 10, std::bind(&SwapNode::yolo_callback, this, _1));
+
+    switch_sub_ = this->create_subscription<std_msgs::msg::Bool>(
+        "use_yolo", 10, std::bind(&SwapNode::use_yolo_callback, this, _1));
+
+    // Publisher
+    pub_output_ = this->create_publisher<std_msgs::msg::String>("output", 10);
+
+    RCLCPP_INFO(this->get_logger(), "SwapNode node started.");
   }
 
 private:
-  void timer_callback() {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+  void intel_callback(const std_msgs::msg::String::SharedPtr msg) {
+    if (!use_yolo) {
+      pub_output_->publish(*msg);
+    }
   }
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+
+  void yolo_callback(const std_msgs::msg::String::SharedPtr msg) {
+    if (use_yolo) {
+      pub_output_->publish(*msg);
+    }
+  }
+
+  void use_yolo_callback(const std_msgs::msg::Bool::SharedPtr msg) {
+    use_yolo = msg->data;
+    RCLCPP_INFO(this->get_logger(), "Switched to %s stream",
+                use_yolo ? "data2" : "data1");
+  }
+
+  // Subscribers
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr intel_sub;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr yolo_sub;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr switch_sub_;
+
+  // Publisher
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_output_;
+
+  // Internal state
+  bool use_yolo;
 };
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<SwapNode>());
   rclcpp::shutdown();
   return 0;
 }
